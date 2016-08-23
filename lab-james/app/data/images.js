ea9fe128eb2c28d26c46804bc69a81854a9c793a
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-08-22 20:03:35 -0700

    added font awesome and did some styling

--- lab-james/app/data/images.js
+++ lab-james/app/data/images.js
@@ -1,1 +1,28 @@
 'use strict';
+
+module.exports = [
+  {
+    id: 1,
+    title: 'Syndrome',
+    description: 'Got it all wrong.',
+    url: 'http://img.lum.dolimg.com/v1/images/image_7b7a8d9b.jpeg'
+  },
+  {
+    id: 2,
+    title: 'Lots\'O Huggin\'',
+    description: 'Hug it out.',
+    url: 'https://pbs.twimg.com/profile_images/1313971811/toy-story-3-lotso-bear_400x400.jpg'
+  },
+  {
+    id: 3,
+    title: 'Sid Phillips',
+    description: 'This kid needs Jesus.',
+    url: 'https://pbs.twimg.com/profile_images/471625645483495424/v-JTwotY_400x400.jpeg'
+  },
+  {
+    id: 4,
+    title: 'Lord Farquuad',
+    description: 'The evilest of them all.',
+    url: 'http://i1.kym-cdn.com/entries/icons/original/000/020/110/Dfdddddd.jpeg'
+  }
+];
